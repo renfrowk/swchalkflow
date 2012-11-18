5475261cd9c76a4cdbf795e257db210e9e048827
Kevin Renfrow <kevin.renfrow@gmail.com>
2012-11-17 19:58:03 -0800

    removed trailing empty list from graph

--- treevisualize/treevisualize.py
+++ treevisualize/treevisualize.py
@@ -18,6 +18,7 @@ def addNode(child, tree):
 		tree[parent_id].append(child_id)
 		addNode(parent, tree)
 
+"""
 #depreciated
 def jsonify(tree):
 	out = json.dumps(tree, sort_keys=True, indent=4)
@@ -44,12 +45,14 @@ def d3node(node, tree):
 	out += "]"
 	out += "}"
 	return out
+"""
 	
 def graphList(tree):
 	out = "["
 	for parent, children in tree.items():
 		for child in children:
-			out += "{source: \"%s\", target: \"%s\", type: \"suit\"}," % (parent[:6], child[:6])
+			if child != []:
+				out += "{source: \"%s\", target: \"%s\", type: \"suit\"}," % (parent[:6], child[:6])
 	out += "]"
 	return out
 
@@ -62,12 +65,10 @@ if __name__ == '__main__':
 	
 	#iterate over all branches
 	#for each branch - needs implementation
-	
-	child = repo.commit('master') #get latest commit
-	
-	commit_tree[child.hexsha].append([]) #add base as parent with no children
-	
-	addNode(child, commit_tree)
+	for branch in repo.branches:
+		child = repo.commit(branch.name) #get latest commit
+		commit_tree[child.hexsha].append([]) #add base as parent with no children
+		addNode(child, commit_tree)
 	
 	print graphList(commit_tree)
 	
