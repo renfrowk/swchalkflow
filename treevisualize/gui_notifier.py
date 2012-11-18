fa5868a3a9047b41efc08c953124d38110e86342
Joey Tomlinson <joey.tomlinson@gmail.com>
2012-11-18 15:18:28 -0800

    gui note stuff

--- treevisualize/gui_notifier.py
+++ treevisualize/gui_notifier.py
@@ -13,6 +13,8 @@ import cgi
 import os
 import tempfile
 import shutil
+import sys
+import StringIO
 
 # Import modules for CGI handling 
 import cgi, cgitb 
@@ -23,41 +25,42 @@ import cgi, cgitb
 #        tmp_dir, temporary directory generated in getSVGFromHash()
 # output: svg file contents
 def cloneTempRepo(commit, tmp_dir):
-	LOCAL_REPO_DIR = "/home/krenfrow/swchalkflow/"
-	SVG_PATH = "/svg_diff/test/" #relative to repo root dir
-	SVG_NAME = "a.svg"
-	
-	#load local repo
-	local_repo = Repo(LOCAL_REPO_DIR, odbt=GitDB) #open the local git repo
-	assert local_repo.bare == False #assert that git repo already exists
-	local_repo.config_reader() #read-only access
-	
-	#ensure tmp_dir exists
-	assert os.path.exists(tmp_dir) == True #ensure dir exists
-	
-	#clone local repo into temp repo
-	temp_repo = local_repo.clone(tmp_dir)
-	
-	#checkout from commit hash
-	temp_repo.git.checkout(commit)
-	
-	f = open(tmp_dir + SVG_PATH + SVG_NAME, 'r')
-	return f.read()
+    LOCAL_REPO_DIR = "/home/tomlinson/swchalkflow/"
+    SVG_PATH = "/svg_diff/test/" #relative to repo root dir
+    SVG_NAME = "a.svg"
+    
+    #load local repo
+    local_repo = Repo(LOCAL_REPO_DIR, odbt=GitDB) #open the local git repo
+    assert local_repo.bare == False #assert that git repo already exists
+    local_repo.config_reader() #read-only access
+    
+    #ensure tmp_dir exists
+    assert os.path.exists(tmp_dir) == True #ensure dir exists
+    
+    #clone local repo into temp repo
+    temp_repo = local_repo.clone(tmp_dir)
+    
+    #checkout from commit hash
+    temp_repo.git.checkout(commit)
+    
+    assert os.path.exists(tmp_dir + SVG_PATH) == True #ensure dir exists
+    f = open(tmp_dir + SVG_PATH + SVG_NAME, 'r')
+    return f.read()
 
 # prints file contents of svg file (hard coded atm) from git snapshot
 #    defined by commit hash
 # input: commit, commit, hash of git commit snapshot to checkout
 # output: prints contents of svg file
 def getSVGFromHash(commit):
-	try:
-		tmp_dir = tempfile.mkdtemp() # create temp dir
-		print cloneTempRepo(commit, tmp_dir)
-	finally:
-		try:
-			shutil.rmtree(tmp_dir) # delete directory
-		except OSError, e:
-			if e.errno != 2: # code 2 - no such file or directory
-				raise
+    try:
+        tmp_dir = tempfile.mkdtemp() # create temp dir
+        return cloneTempRepo(commit, tmp_dir)
+    finally:
+        try:
+            shutil.rmtree(tmp_dir) # delete directory
+        except OSError, e:
+            if e.errno != 2: # code 2 - no such file or directory
+                raise
 
 def addNode(child, tree):
     #base case
@@ -74,20 +77,161 @@ def addNode(child, tree):
 
 # generate list of commit tree for D3 graph
 def graphList(tree):
-	out = "["
-	for parent, children in tree.items():
-		for child in children:
-			if child != []:
-				out += "{\"source\": \"%s\", \"target\": \"%s\", \"type\": \"suit\"}," % (parent[:6], child[:6])
-	out = out[:-1] #remove trailing comma
-	out += "]"
-	return out
+    out = "["
+    for parent, children in tree.items():
+        for child in children:
+            if child != []:
+                out += "{\"source\": \"%s\", \"target\": \"%s\", \"type\": \"suit\"}," % (parent[:6], child[:6])
+    out = out[:-1] #remove trailing comma
+    out += "]"
+    return out
 
 # Create instance of FieldStorage 
 form = cgi.FieldStorage() 
 
 # Get data from get fields
 command = form.getvalue('command')
+
+
+
+
+
+
+
+
+
+
+def getTagBounds (lineArr, startString, endString):
+    
+    blocks = []
+    startI = []
+    endI = []
+
+    for lineNum, line in enumerate(lineArr):
+        if(line.find(startString) != -1):
+            startI.append(lineNum)
+        if(line.find(endString) != -1):
+            endI.append(lineNum)
+
+    for i, bla in enumerate(startI):
+        blocks.append({'start': startI[i], 'end': endI[i]})
+        
+    return blocks;
+
+def parseGs (fileArr):
+
+    gBounds = getTagBounds(fileArr, "<g", "</g>");
+
+    #pprint(gBounds)
+
+    gs = []
+
+    #pprint(gBounds)
+    for gI,gBound in enumerate(gBounds):
+        gString = fileArr[gBound['start']+1:gBound['end']]
+        #pprint(gString)
+        pathBounds = getTagBounds(gString, "<path", "inkscape:connector-curvature");
+        thisG = {'bounds': gBound}
+        thisG['paths'] = [];
+        for pathBound in pathBounds:
+            pathString = gString[pathBound['start']:pathBound['end']+1]
+            thisG['paths'].append({'strings': pathString, 'bounds': pathBound, 'id': int(pathString[3][15:19])})
+            #pprint(pathString)
+        gs.append(thisG)
+
+    return gs
+
+def findPathId(g,pathId):
+    for path in g['paths']:
+        if (pathId == path['id']):
+            return True;
+    return False;
+
+def findAdditions(orig, new, added, deleted, unchanged):
+    for path in orig['paths']:
+        if (findPathId(new, path['id']) == False):
+            deleted.append(path)
+        else:
+            unchanged.append(path)
+    for path in new['paths']:
+        if (findPathId(orig, path['id']) == False):
+            added.append(path)
+
+def generateDiffFile (origString, newString, fileName):
+
+    newStringBuf = StringIO.StringIO(newString)
+    origStringBuf = StringIO.StringIO(origString)
+    origLines = origStringBuf.readlines()
+    newLines = newStringBuf.readlines()
+
+    orig = parseGs(origLines)[0]
+    new = parseGs(newLines)[0]
+
+    added = []
+    deleted = []
+    unchanged = []
+
+    findAdditions(orig, new, added, deleted, unchanged)
+
+    #~ print('ADDED\n')
+    #~ pprint(added)
+
+    #~ print('DELETED\n')
+    #~ pprint(deleted)
+
+    #~ print('UNCHANGED\n')
+    #~ pprint(unchanged)
+
+    gBounds = getTagBounds(origLines, "<g", "</g>")
+    prefix = origLines[0:gBounds[0]['start']]
+    suffix = origLines[gBounds[-1]['end']+1:]
+
+    addedDocLines = []
+    deletedDocLines = []
+    unchangedDocLines = []
+
+    unchangedDocLines.append('  <g\n')
+    unchangedDocLines.append('    inkscape:label="Chalkflow_Unchanged"\n')
+    unchangedDocLines.append('    inkscape:groupmode="layer"\n')
+    unchangedDocLines.append('    id="layer1">\n')
+
+    addedDocLines.append('  <g\n')
+    addedDocLines.append('    inkscape:label="Chalkflow_Added"\n')
+    addedDocLines.append('    inkscape:groupmode="layer"\n')
+    addedDocLines.append('    id="layer2">\n')
+
+    deletedDocLines.append('  <g\n')
+    deletedDocLines.append('    inkscape:label="Chalkflow_Deleted"\n')
+    deletedDocLines.append('    inkscape:groupmode="layer"\n')
+    deletedDocLines.append('    id="layer3">\n')
+
+    for path in added:
+        addedDocLines += path['strings']
+        
+    for path in deleted:
+        deletedDocLines += path['strings']
+        
+    for path in unchanged:
+        unchangedDocLines += path['strings']
+
+    addedDocLines.append('  </g>\n')
+    deletedDocLines.append('  </g>\n')
+    unchangedDocLines.append('  </g>\n')
+
+    output = prefix + unchangedDocLines + addedDocLines + deletedDocLines + suffix
+
+    outputF = open(fileName, 'w')
+
+    for line in output:
+        outputF.write(line)
+        
+    outputF.close()
+    
+    
+    
+
+
+
 
 print "Content-type:text/html\r\n\r\n"
 
@@ -149,15 +293,18 @@ if (command == 'poll_tree_change'):
 if (command == 'view'):
     form = cgi.FieldStorage()
     hexsha = form.getvalue('hexsha')
-    print 'VIEW ' + hexsha
     svg_file = getSVGFromHash(hexsha) #svg_file will be string of file contents
+    outputF = open('/home/tomlinson/output/'+hexsha+'.svg', 'w')
+    outputF.write(svg_file)
+    outputF.close()
+    
     # set head to hexsha, open inkscape with document (with listener)
     
 if (command == 'diff'):
     form = cgi.FieldStorage()
     hexsha_orig = form.getvalue('hexsha_orig')
     hexsha_new = form.getvalue('hexsha_new')
-    print 'DIFF ' + hexsha_orig + ' -> ' + hexsha_new
     svg_orig = getSVGFromHash(hexsha_orig)
     svg_new = getSVGFromHash(hexsha_new)
+    generateDiffFile(svg_orig, svg_new, '/home/tomlinson/output/'+hexsha_orig+'_'+hexsha_new+'.svg')
     # compute diff, open inkscape window (with listener)
